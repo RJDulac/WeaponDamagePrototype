ceaa2f94eba762d410a250f5eb1b00651329716a
Ryan Dulac <analyticrye@gmail.com>
2022-02-11 13:48:59 -0500

    Add project files.

---
+++ WeapondamagePrototype/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeapondamagePrototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
